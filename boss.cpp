9eb2c57a3ede74a72f44004eb9b04f8a48471630
joo8070 <68212702+joo8070@users.noreply.github.com>
2020-07-20 14:22:58 +0900

    ADD boss & testBossScene

--- boss.cpp
+++ boss.cpp
@@ -1,2 +1,26 @@
 #include "stdafx.h"
 #include "boss.h"
+
+HRESULT boss::init()
+{
+	// ------------ 임시 변수 ------------ //
+	_x = _y = _jumpPower = _gravity = 0;
+	_state = BOSS_LEFT_IDLE;
+	_rc = RectMakeCenter(_x, _y, 100, 100);
+	
+
+	// ------------ 임시 변수 ------------ //
+	return S_OK;
+}
+
+void boss::release()
+{
+}
+
+void boss::render()
+{
+}
+
+void boss::update()
+{
+}
--- boss.h
+++ boss.h
@@ -1,7 +1,35 @@
 #pragma once
 #include "gameNode.h"
-class boss :
-	public gameNode
+
+enum STATEBOSS
 {
+	BOSS_LEFT_IDLE,       BOSS_RIGHT_IDLE,
+	BOSS_LEFT_WALK,       BOSS_RIGHT_WALK,
+	BOSS_LEFT_HIT,        BOSS_RIGHT_HIT,
+	BOSS_LEFT_HIT_GETUP,  BOSS_RIGHT_HIT_GETUP,
+	BOSS_LEFT_ATTACK,     BOSS_RIGHT_ATTACK,
+	BOSS_LEFT_DEATH,      BOSS_RIGHT_DEATH,
+	BOSS_LEFT_BLOCK,      BOSS_RIGHT_BLOCK,
+	BOSS_LEFT_
+};
+
+
+class boss : public gameNode
+{
+private:		
+	float      _x, _y;                 // 보스 중심점
+	float      _jumpPower, _gravity;   // 보스 점프 관련 변수		
+	STATEBOSS  _state;                 // 보스 상태 저장용
+	RECT       _rc;                    // 보스 충돌 렉트
+	image*     _img;                   // 보스 이미지
+		
+public:
+	boss() {}
+	~boss() {}
+public:
+	HRESULT init();
+	void    release();
+	void    render();
+	void    update();
 };
 
