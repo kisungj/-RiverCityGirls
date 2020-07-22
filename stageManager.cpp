4442c0fe19854c11b5fbc4bf89f81a620242aeef
송용은 <song_6628@naver.com>
2020-07-22 17:05:57 +0900

    스테이지 만짐

--- stageManager.cpp
+++ stageManager.cpp
@@ -25,15 +25,22 @@ HRESULT stageManager::init()
 	_enemyManager = new enemyManager;
 	_boss	      =	new boss;
 	_stage1 = new stage1;
-	_stage1->init(_obstacleManager, _itemManager, _enemyManager, _player);
+	//_stage1->init(_obstacleManager, _itemManager, _enemyManager, _player);
 
 	_stage2 = new stage2;
-	_stage2->init(_obstacleManager, _itemManager, _enemyManager, _player);
-
+	//_stage2->init(_obstacleManager, _itemManager, _enemyManager, _player);
+	
 	_stageBoss = new stageBoss;
-	_stageBoss->init(_player,_boss);
+	//_stageBoss->init(_player,_boss);
+
+	_title = new titleStage;
+	_title->init();
+
+	SCENEMANAGER->addScene("TITLE_SCENE", _title);
+	_curStageName = "TITLE_SCENE";
+	SCENEMANAGER->changeScene(_curStageName);
+
 	SCENEMANAGER->addScene("STAGE1_SCENE", _stage1);
-	SCENEMANAGER->changeScene("STAGE1_SCENE");
 
 	SCENEMANAGER->addScene("STAGE2_SCENE", _stage2);
 
@@ -61,28 +68,37 @@ void stageManager::render()
 
 	_player->render();
 
-	_ui->render();
+	if(_curStageName != "TITLE_SCENE")
+		_ui->render();
 }
 
 void stageManager::update()
 {
 	SCENEMANAGER->update();
 
-	for (int i = 0; i < _itemManager->getVItem().size(); i++)
-	{
-		_itemManager->getVItem()[i]->update();
-	}
-	for (int i = 0; i < _obstacleManager->getVObstacle().size(); i++)
-	{
-		_obstacleManager->getVObstacle()[i]->update();
-	}
-	for (int i = 0; i < _enemyManager->getVBoy().size(); i++)
-	{
-		_enemyManager->getVBoy()[i]->update();
-		_enemyManager->getVBoy()[i]->directionCheck(_player->getPlayerRect(), _player->getPlayerX(), _player->getPlayerY());
-	}
 	_ui->update();
-	_player->update();
+
+	if (!_ui->getIsPhone())
+	{
+		_player->update();
+
+		for (int i = 0; i < _itemManager->getVItem().size(); i++)
+		{
+			_itemManager->getVItem()[i]->update();
+		}
+
+		for (int i = 0; i < _obstacleManager->getVObstacle().size(); i++)
+		{
+			_obstacleManager->getVObstacle()[i]->update();
+		}
+
+		for (int i = 0; i < _enemyManager->getVBoy().size(); i++)
+		{
+			//_enemyManager->getVBoy()[i]->update();
+			_enemyManager->getVBoy()[i]->directionCheck(_player->getPlayerRect(), _player->getPlayerX(), _player->getPlayerY());
+		}
+
+	}
 	// ================================================================
 	//if (KEYMANAGER->isOnceKeyDown(VK_F1)) SCENEMANAGER->changeScene("PLAYER_SCENE");
 	//
@@ -94,19 +110,29 @@ void stageManager::update()
 	//
 	//if (KEYMANAGER->isOnceKeyDown(VK_F5)) SCENEMANAGER->changeScene("OBSTACLE_SCENE");
 
+	if (_curStageName == "TITLE_SCENE")
+	{
+		if (_title->getNextScene())
+		{
+			_obstacleManager->release();
+			_enemyManager->release();
+			_itemManager->release();
+			SCENEMANAGER->changeScene("STAGE1_SCENE");
+			_stage1->init(_obstacleManager, _itemManager, _enemyManager, _player);
+			_title->setNextScene(false);
+			_curStageName = "STAGE1_SCENE";
+		}
+	}
+	
+
 	if (KEYMANAGER->isOnceKeyDown(VK_F1))
 	{
 		_obstacleManager->release();
 		_enemyManager->release();
 		_itemManager->release();
 		SCENEMANAGER->changeScene("STAGE1_SCENE");
-		_obstacleManager->init();
-		_enemyManager->init();
-		_itemManager->init();
-		//---- 임시
-		_obstacleManager->setObstacle(DESK);
-		_enemyManager->setBoy();
-		//---- 임시
+		_stage1->init(_obstacleManager, _itemManager, _enemyManager, _player);
+		_curStageName = "STAGE1_SCENE";
 	}
 	if (KEYMANAGER->isOnceKeyDown(VK_F2))
 	{
@@ -114,6 +140,8 @@ void stageManager::update()
 		_enemyManager->release();
 		_itemManager->release();
 		SCENEMANAGER->changeScene("STAGE2_SCENE");
+		_stage2->init(_obstacleManager, _itemManager, _enemyManager, _player);
+		_curStageName = "STAGE2_SCENE";
 	}
 	if (KEYMANAGER->isOnceKeyDown(VK_F3))
 	{
@@ -122,7 +150,8 @@ void stageManager::update()
 		_itemManager->release();
 		_boss->init();
 		SCENEMANAGER->changeScene("STAGEBOSS_SCENE");
-
+		_stageBoss->init(_player, _boss);
+		//_curStageName = "STAGEBOSS_SCENE";
 	}
 }
 
