06a8fd8522831b9e51a368bfe6487babac0b1940
송용은 <song_6628@naver.com>
2020-07-22 12:17:35 +0900

    다이얼로그 완성

--- testUIScene.cpp
+++ testUIScene.cpp
@@ -3,30 +3,24 @@
 
 HRESULT testUIScene::init()
 {
+	IMAGEMANAGER->addImage("s_boss_name", "image/ui/small_boss_name.bmp", 143, 38, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("s_kyoko_name", "image/ui/small_kyoyo_name.bmp", 143, 38, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("s_misako_name", "image/ui/small_misako_name.bmp", 143, 38, true, RGB(255, 0, 255));
+
+	IMAGEMANAGER->addImage("kyoko_img", "image/ui/kyoko_img.bmp", 600, 600, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("misuzu_img", "image/ui/misuzu_img.bmp", 600, 600, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("misako_img", "image/ui/misako_img.bmp", 600, 600, true, RGB(255, 0, 255));
+
 	_ui = new ui;
 	_ui->init();
-
-	//INIDATA->addData("TALK", "TALK1", "M미스즈, 방해하지 마!");
-	//INIDATA->addData("TALK", "TALK2", "K그래. 널 다치게 하긴 싫어.");
-	//INIDATA->addData("TALK", "TALK3", "B날 다치게 해? 하하, 재밌는데 그래. 너희들처럼 비쩍 마른 새끼고양이들은 파리도 못 죽일 걸.");
-	//INIDATA->addData("TALK", "TALK4", "M무슨 소리야! 파리 정도는 쉽게 죽일 수 있어!");
-	//INIDATA->addData("TALK", "TALK5", "B경비원으로서, 너희들 같은 바보가 수업을 땡땡이치지 않게 감시하는 게 내 일이거든.");
-	//INIDATA->addData("TALK", "TALK6", "K그치만 너 이 학교 학생도 아니잖아.");
-	//INIDATA->addData("TALK", "TALK7", "B요게...닥쳐! 난 돈이 필요해!!");
-	//INIDATA->addData("TALK", "TALK8", "K으아!!");
-	//INIDATA->addData("TALK", "TALK9", "B나보다 머리 좋은 척하지 마! 난 고등학교도 7년째 다니고 있다고! 이 미스즈보다 학교를 잘 아는 놈은 없어!");
-	//INIDATA->addData("TALK", "TALK10", "M네가 아무리 크고 바보라도 상관없어. 우린 이 학교를 나갈 거니까! 지금 당장!");
-	//INIDATA->addData("TALK", "TALK11", "K맞아, 쿠니오랑 리키를 구하러 갈 거야!");
-	//INIDATA->addData("TALK", "TALK12", "M너는 벽이나 뚫고 다니면서 놀고 있으란 말야, 문이란 게 뭔지 알 때까지.");
-	//INIDATA->addData("TALK", "TALK13", "B이것들이, 더 못 참겠다! 망항 자식들...두들겨 패버리겠어!");
-
-
-	//INIDATA->iniSave("TALK");
 
 	_index = 1;
 	_chatCount = 2;
 	
+	_isTalk = true;
 
+	_playerX = -200;
+	_bossX = 1300;
 
 	return S_OK;
 }
@@ -37,38 +31,85 @@ void testUIScene::render()
 
 	SetBkMode(getMemDC(), TRANSPARENT);
 	HFONT font, oldFont;
-	RECT rcText = RectMake(300, WINSIZEY - 100, 1000, 700);
+	RECT rcText = RectMake(400, WINSIZEY - 75, 1100, 400);
 
 	font = CreateFont(30, 0, 0, 0, 100, 0, 0, 0, HANGUL_CHARSET, 0, 0, 0, 0, TEXT("HY견고딕"));
 	oldFont = (HFONT)SelectObject(getMemDC(), font);
 	SetTextColor(getMemDC(), RGB(255, 255, 255));
-	DrawText(getMemDC(), TEXT(_test.c_str()), _chatCount, &rcText, DT_LEFT | DT_WORDBREAK);
+	if (_isTalk)
+	{
+		IMAGEMANAGER->findImage("phone_active")->alphaRender(getMemDC(), 100);
+		if (_typeStr == "B")
+		{
+			IMAGEMANAGER->findImage("s_boss_name")->render(getMemDC(),200, WINSIZEY - 80);
+			IMAGEMANAGER->findImage("misuzu_img")->render(getMemDC(), _bossX, 200);
+		}
+		if (_typeStr == "M")
+		{
+			IMAGEMANAGER->findImage("s_misako_name")->render(getMemDC(), 200, WINSIZEY - 80);
+			IMAGEMANAGER->findImage("misako_img")->render(getMemDC(), _playerX, 210);
+		}
+		if (_typeStr == "K")
+		{
+			IMAGEMANAGER->findImage("s_kyoko_name")->render(getMemDC(), 200, WINSIZEY - 80);
+			IMAGEMANAGER->findImage("kyoko_img")->render(getMemDC(), _playerX, 210);
+		}
+		DrawText(getMemDC(), TEXT(_firstNotStr.c_str()), _chatCount, &rcText, DT_LEFT | DT_WORDBREAK);
+	}
+	SelectObject(getMemDC(),oldFont);
+	DeleteObject(font);
+
 }
 
 void testUIScene::update()
 {
-	_ui->update();
+	if(!_isTalk)
+		_ui->update();
 
-	if (_index != 13)
+
+	if (_isTalk)
 	{
+	
 		if (KEYMANAGER->isOnceKeyDown('I'))
 		{
+			if (_index == 13)
+			{
+				_isTalk = false;
+				return;
+			}
 			_index += 1;
 			_chatCount = 0;
+			_playerX = -200;
+			_bossX = 1300;
 		}
-	}
+		
+		//if (_index == 13 && _chatCount == strlen(_test.c_str()))
+		//{
+		//	_isTalk = false;
+		//}
 
-	sprintf_s(_indexStr, "TALK%d", _index);
-	_test = INIDATA->loadDataString("TALK", "TALK", _indexStr);
+		sprintf_s(_indexStr, "TALK%d", _index);
+		_test = INIDATA->loadDataString("TALK", "TALK", _indexStr);
 
-	//cout << _test.front() << endl;
-	//_test.erase(_test.front());
-	cout << _test.substr(1,_test.length()) << endl;
+		//cout << _test.front() << endl;
+		//_test.erase(_test.front());
+		_typeStr = _test.front();
+		_firstNotStr = _test.substr(1, _test.length());
 
 
-	if (_chatCount < strlen(_test.c_str()))
-	{
-		_chatCount += 0.5f;
+		if (_chatCount < strlen(_test.c_str()))
+		{
+			_chatCount += 0.5f;
+		}
+
+		if (_playerX < 0)
+		{
+			_playerX+= 5;
+		}
+		if (_bossX > 1000)
+		{
+			_bossX-= 5;
+		}
 	}
 }
 
